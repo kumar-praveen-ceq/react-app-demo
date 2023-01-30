34c6bffd548e135eb03bf3ea3e0ab42c817b2c63
unknown <praveenkumar.s@cloudeq.com>
2023-01-30 11:22:03 +0530

    Update workflow and removed element from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { TodoContextProvider } from "./store/todos-context";
 function App() {
   return (
     <TodoContextProvider>
-      <h1>Hello World!</h1>
       <NewTodo />
       <Todos />
     </TodoContextProvider>
